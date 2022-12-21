d1e0453dda91a39d8e314faec4a4539c6b02e761
rudrodip <official.rudrodipsarker@gmail.com>
2022-12-21 13:57:23 +0600

    update

--- TextProcessing/process.py
+++ TextProcessing/process.py
@@ -11,7 +11,7 @@ class TextProcessor:
             model="text-davinci-003",
             prompt=prompt,
             temperature=0.5,
-            max_tokens=2000,
+            max_tokens=500,
             top_p=0.3,
             frequency_penalty=0.5,
             presence_penalty=0,
