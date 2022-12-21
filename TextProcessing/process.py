b7445992af24016f4802762ddd65e03f13aa2ddb
rudrodip <official.rudrodipsarker@gmail.com>
2022-12-21 10:52:50 +0600

    initial commit

---
+++ TextProcessing/process.py
@@ -0,0 +1,68 @@
+import openai
+import os
+import json
+
+OPEN_AI_TOKEN = os.environ['OPENAI']
+openai.api_key = OPEN_AI_TOKEN
+
+class TextProcessor:
+    def text_generator(self, prompt):
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=200,
+            top_p=0.3,
+            frequency_penalty=0.5,
+            presence_penalty=0,
+        )
+        try:
+            answer = response.choices[0].text.strip()
+        except:
+            answer = None
+        return answer
+
+
+    def image_generator(self, prompt):
+        response = openai.Image.create(prompt=prompt, n=2, size="512x512")
+        image_urls = response["data"]
+        for url in image_urls:
+            yield url['url']
+
+class Response(TextProcessor):
+    chat_log_path = "./TextProcessing/Database/chat_log.txt"
+    def __init__(self, config_path):
+        # getting the config file
+        with open(config_path, 'r') as f:
+            self.config = json.load(f)
+
+        # getting previous chat log
+        with open(self.chat_log_path, 'r', encoding="utf-8") as f:
+            self.chat_log = f.read()
+
+    def get_context(self):
+        personal_context = self.config["personal_context"]
+        chat_log = self.chat_log
+
+        context = f'{personal_context}\n{chat_log}'
+        return context
+
+    def create_context(self, author, text, response):
+        with open(self.chat_log_path, 'a', encoding="utf-8") as f:
+            f.write(f"\n{author}: {text}\n{response}")
+        
+    def get_response(self, text):
+        if text == "hi":
+            return "Hi ! 💙 😊"
+
+        author = self.config["author_name"]
+        bot = self.config["bot_name"]
+
+        context = self.get_context()
+        prompt = f"{context}\n{author} : {text}"
+        response = self.text_generator(prompt)
+
+        self.create_context(author, text, response)
+
+        response = response.replace(f'{bot}:', '').strip()
+        return response
---
+++ bot.py
@@ -0,0 +1,49 @@
+import io
+import discord
+import os
+import aiohttp
+from TextProcessing import process
+
+TOKEN = os.environ["BABE"]
+PERSONAL_ID = 841126921886498817
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix="!", intents=intents)
+response_generator = process.Response('./TextProcessing/Database/config.json')
+
+async def send_img(message, url):
+    async with aiohttp.ClientSession() as session: # creates session
+            async with session.get(url) as resp: # gets image from url
+                img = await resp.read() # reads image from response
+                with io.BytesIO(img) as file: # converts to file-like object
+                    await message.channel.send(file=discord.File(file, "imagination.png"))
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user and message.author.id != PERSONAL_ID:
+        pass
+
+    if message.content.startswith("self profile"):
+        await message.channel.send(file=discord.File("babe.png"))
+
+    if message.content.startswith("imagine"):
+        prompt = message.content.lstrip("imagine:").strip()
+        await message.channel.send("Generating images....")
+        images = response_generator.image_generator(prompt)
+        for img in images:
+            await send_img(message, img)
+
+        await message.channel.send(prompt)
+
+    prompt = message.content
+    if message.author.id == PERSONAL_ID:
+        response = response_generator.get_response(prompt)
+        if response:
+            await message.channel.send(response)
+
+client.run(TOKEN)
